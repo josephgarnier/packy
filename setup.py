1672a90abf299a1c7b05c4001b7958773bcc27b4
Joseph Garnier <garnjose@gmail.com>
2026-07-29 22:54:37 +0200

    Clean project

--- setup.py
+++
@@ -1,9 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-# base="Win32GUI" should be used only for Windows GUI app
-base = "Win32GUI" if sys.platform == "win32" else None
-
-sys.path.append("./packy/")
-
-setup(executables=[Executable("packy/main.py", base=base)])
